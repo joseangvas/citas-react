4302048bab03b6d1e6aa832e9580b140d82e3da1
Joseangvas <developer.javp@gmail.com>
2023-12-22 10:34:56 -0400

    Crear Carpeta Components en src.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 function App() {
+  const sumar = () => {
+    const a = 10;
+    const b = 30;
+
+    return a + b;
+  }
+
+  const total = sumar();
+
   return (
     <>
       <div className="App">
-        <h1>Hola Mundo</h1>
+        <h1>{'Hola Mundo'.toUpperCase()}</h1>
+        <h5>{`La suma es: ${total}`}</h5>
       </div>
     </>
   )
