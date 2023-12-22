0fe5867702555c10dad47708b337700dbb36a527
Joseangvas <developer.javp@gmail.com>
2023-12-22 16:12:16 -0400

    Crear Componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
-  const sumar = () => {
-    const a = 10;
-    const b = 30;
 
-    return a + b;
-  }
-
-  const total = sumar();
 
   return (
     <>
-      <div className="App">
-        <h1>{'Hola Mundo'.toUpperCase()}</h1>
-        <h5>{`La suma es: ${total}`}</h5>
-      </div>
+      <Header />
+
+
     </>
   )
 }
