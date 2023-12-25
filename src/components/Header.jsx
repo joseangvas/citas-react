cf0167491bc55f70a072787fafbfae77f98f7760
Joseangvas <developer.javp@gmail.com>
2023-12-25 00:22:29 -0400

    Diseñar Titulo de Formulario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,10 @@ function Header() {
 
 
   return (
-    <>
-      <h1 className="font-black text-4xl text-center w-1/2 mx-auto">
-        Seguimiento Pacientes {''}
+      <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+        Seguimiento Pacientes {""}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
-    </>
   )
 }
 
