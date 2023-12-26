6fa6b6929784a2fd261da9df1a1d502fca7910f9
Joseangvas <developer.javp@gmail.com>
2023-12-26 13:38:32 -0400

    Aplicando Tailwind al Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,8 +8,8 @@ const Formulario = () => {
           <span className="text-indigo-600 font-bold">Administrarlos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
-        <div>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota
           </label>
@@ -20,9 +20,63 @@ const Formulario = () => {
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           />
-
-          
         </div>
+
+        <div className="mb-5">
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+            Nombre Propietario
+          </label>
+
+          <input
+            id="propietario"
+            type='text'
+            placeholder="Nombre del Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+            Email
+          </label>
+
+          <input
+            id="email"
+            type='email'
+            placeholder="Email Contacto Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold">
+            Fecha
+          </label>
+
+          <input
+            id="fecha"
+            type='date'
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+            Síntomas
+          </label>
+
+          <textarea
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los Síntomas"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer rounded-md transition-all"
+          value="Agregar Paciente"
+        />
       </form>
     </div>
   )
