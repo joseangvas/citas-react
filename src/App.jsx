c4d6db9b127aa90fd505a4a7f51b474ee4048c78
Joseangvas <developer.javp@gmail.com>
2023-12-26 11:16:20 -0400

    Inicio de Código de Formulario Mascotas

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ function App() {
   return (
     <div className="container mx-auto mt-10">
       <Header />
-      <Formulario />
-      <ListadoPacientes />
-      <Estilos />
+
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+
+      </div>
 
     </div>
   )
