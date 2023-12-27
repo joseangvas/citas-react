128f1dc8fd5461c27800b22c3d6c7b232ac1f755
Joseangvas <developer.javp@gmail.com>
2023-12-27 15:42:19 -0400

    Codificar Formulario de Ingreso de Pacientes.

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,13 @@
 const ListadoPacientes = () => {
   return (
     <div className="w-1/2 lg:w-3/5">
-      <h2>ListadoPacientes</h2>
+      <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
+
+      
     </div>
   )
 }
