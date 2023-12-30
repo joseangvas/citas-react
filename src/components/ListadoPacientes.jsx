738251df3fb8a085b5108f265f41630f071ae8fa
Joseangvas <developer.javp@gmail.com>
2023-12-30 19:59:08 -0400

    Usar Props children

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -11,6 +11,13 @@ const ListadoPacientes = ({pacientes}) => {
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
 
+      { pacientes.map( () => {
+
+        return (
+          <h1>Desde Map</h1>
+        )
+      })}
+
       <Paciente />
 
     </div>
