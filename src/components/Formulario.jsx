2e3487e83d300881014b2f81f9c65d400426030a
Joseangvas <developer.javp@gmail.com>
2023-12-29 20:47:41 -0400

    Crear Estado en Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,13 @@
+import {useState, useEffect} from 'react';
+
 const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -8,7 +17,9 @@ const Formulario = () => {
           <span className="text-indigo-600 font-bold">Administrarlos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form 
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota
@@ -19,6 +30,8 @@ const Formulario = () => {
             type='text'
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
