38f462ff7e65d873d819db03bf0a970f12841c72
Joseangvas <developer.javp@gmail.com>
2023-12-31 05:03:10 -0400

    Crear botón Eliminar Paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -24,8 +24,16 @@ const Paciente = ({paciente}) => {
         <span className="font-normal normal-case">{sintomas}</span>
       </p>
 
-      <div>
-        
+      <div className="flex justify-between mt-10 px-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white font-bold uppercase rounded-lg"
+        >Editar</button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-lg"
+        >Eliminar</button>
       </div>
     </div>
   )
