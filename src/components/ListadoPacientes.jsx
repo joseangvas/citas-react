96938a363e3c927dfbe80e62f4e0f14f3b3cfa30
Joseangvas <developer.javp@gmail.com>
2023-12-31 04:25:33 -0400

    Validar si existen pacientes en el Listado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -22,10 +22,10 @@ const ListadoPacientes = ({pacientes}) => {
         </>
       ) : (
         <>
-          <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
-           <p className="text-xl mt-5 mb-10 text-center">
-            Administra tus {''}
-            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          <h2 className="font-black text-3xl text-center">No Hay Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
           </p>
         </>
       )}
