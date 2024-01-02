04ec4a586e8c9907c93499f34b1e882db174e5f6
Joseangvas <developer.javp@gmail.com>
2024-01-02 07:49:40 -0400

    Crear Hook para Eliminar Paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,27 +1,31 @@
-const Paciente = ({paciente, setPaciente}) => {
-
+const Paciente = ({ paciente, setPaciente }) => {
   // Aplicamos Destructuring
-  const {nombre, propietario, email, fecha, sintomas} = paciente
+  const { nombre, propietario, email, fecha, sintomas } = paciente;
 
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
         <span className="font-normal normal-case">{nombre}</span>
       </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
         <span className="font-normal normal-case">{propietario}</span>
       </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
         <span className="font-normal normal-case">{email}</span>
       </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
         <span className="font-normal normal-case">{fecha}</span>
       </p>
 
-      <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}
         <span className="font-normal normal-case">{sintomas}</span>
       </p>
 
@@ -30,15 +34,20 @@ const Paciente = ({paciente, setPaciente}) => {
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white font-bold uppercase rounded-lg"
           onClick={() => setPaciente(paciente)}
-        >Editar</button>
+        >
+          Editar
+        </button>
 
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-lg"
-        >Eliminar</button>
+          onClick={() => {}}
+        >
+          Eliminar
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Paciente;
