118243bb32870dd2c0db05ce198f9fad16b033a4
Joseangvas <developer.javp@gmail.com>
2024-01-02 08:09:50 -0400

    Codificar Función para Eliminar Paciente

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
-  const eliminarPaciente = (id) => {
+  const eliminarPaciente = id => {
     const pacientesActualizados = pacientes.filter(
       (paciente) => paciente.id !== id
     );
