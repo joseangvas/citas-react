26ded8e48bff502da235574285f2749bb391853d
Joseangvas <developer.javp@gmail.com>
2024-01-02 07:54:35 -0400

    Transferir Hook de Eliminar Paciente al Listado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,23 +1,22 @@
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
-
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-
       {pacientes && pacientes.length ? (
         <>
           <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
-            Administra tus {''}
+            Administra tus {""}
             <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
           </p>
 
-          {pacientes.map( paciente => (
+          {pacientes.map((paciente) => (
             <Paciente
-              key = {paciente.id}
-              paciente = {paciente}
-              setPaciente = {setPaciente}
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
             />
           ))}
         </>
@@ -25,13 +24,15 @@ const ListadoPacientes = ({pacientes, setPaciente}) => {
         <>
           <h2 className="font-black text-3xl text-center">No Hay Pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
-            Comienza agregando pacientes {''}
-            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar
+            </span>
           </p>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default ListadoPacientes;
