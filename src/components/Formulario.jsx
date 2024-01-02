a5b2f3e57e044c723d4eb6bf988d538b77688184
Joseangvas <developer.javp@gmail.com>
2024-01-02 07:31:31 -0400

    Validar si se edita un Paciente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -45,26 +45,25 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
       propietario,
       email,
       fecha,
-      sintomas
+      sintomas,
     };
 
-    if(paciente.id)
-      // Editando el Registro 
+    if (paciente.id) {
+      // Editando el Registro
       objetoPaciente.id = paciente.id;
 
-      const pacientesActualizados = pacientes.map(pacienteState => pacientesState.id === paciente.id ? objetoPaciente : pacienteState);
+      const pacientesActualizados = pacientes.map((pacienteState) =>
+        pacienteState.id === paciente.id ? objetoPaciente : pacienteState
+      );
 
       setPacientes(pacientesActualizados);
       setPacientes({});
-
     } else {
       // Nuevo Registro
       objetoPaciente.id = generarId();
+      // Duplicar el Arreglo Original actualizado con Valores del Form
       setPacientes([...pacientes, objetoPaciente]);
     }
-
-    // Duplicar el Arreglo Original actualizado con Valores del Form
-    setPacientes([...pacientes, objetoPaciente]);
 
     // Reiniciar el Formulario
     setNombre("");
