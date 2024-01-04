825379a2e9062c45a84a44168b88fad35b50fc6c
Joseangvas <developer.javp@gmail.com>
2024-01-04 19:01:42 -0400

    Incorporar localStorage en el Proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect} from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -7,35 +7,35 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
-  // Obtener lo que haya en localStorage
   useEffect(() => {
     const obtenerLS = () => {
-      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
       setPacientes(pacientesLS);
-    }
+    };
     obtenerLS();
   }, []);
 
-  // Guardar el Paciente en LocalStorage
   useEffect(() => {
-    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
   }, [pacientes]);
 
-  const eliminarPaciente = id => {
-    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
     setPacientes(pacientesActualizados);
   };
 
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-20">
       <Header />
 
       <div className="mt-12 md:flex">
         <Formulario
-          pacientes={pacientes} // Asignar Props
-          setPacientes={setPacientes} // Asignar Props
+          pacientes={pacientes}
+          setPacientes={setPacientes}
           paciente={paciente}
-          setPaciente={setPaciente} // Asignar Props
+          setPaciente={setPaciente}
         />
         <ListadoPacientes
           pacientes={pacientes}
@@ -47,4 +47,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
