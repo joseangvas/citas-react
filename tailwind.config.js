29684f167b56be201125a7d637b0c818850a6657
Joseangvas <developer.javp@gmail.com>
2024-01-04 19:14:29 -0400

    Hacer Deplypment del Proyecto.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["index.html", "./src/**/*.jsx"],
+  darkmode: false,
+  variants: {
+    extends: {},
+  },
   theme: {
     extend: {},
   },
