1a42ef519eac1d1218a987ef6224521237092a34
Joseangvas <developer.javp@gmail.com>
2024-01-04 16:56:48 -0400

    Correcciones en ListadoPacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       {pacientes && pacientes.length ? (
