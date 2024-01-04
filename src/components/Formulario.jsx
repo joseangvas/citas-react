db9ff5d5fba884c01e2811d3ebfdb3b92545f26a
Joseangvas <developer.javp@gmail.com>
2024-01-04 16:59:23 -0400

    Incorporar LocalStorage en el Proyecto.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import Error from "./Error";
 
 const Formulario = ({ pacientes, setPacientes, paciente }) => {
