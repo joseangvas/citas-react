0bf85c58f00c030d6550f49d11505749f5154944
Joseangvas <developer.javp@gmail.com>
2024-01-04 17:21:18 -0400

    Incorporar LocalStorage en el Proyecto.

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,8 +1,10 @@
 const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
   // Aplicamos Destructuring
   const { nombre, propietario, email, fecha, sintomas, id } = paciente;
+  
   const handleEliminar = () => {
     const respuesta = confirm('Deseas Eliminar este Paciente');
+
     if(respuesta) {
       eliminarPaciente(id);
     }
@@ -11,27 +13,27 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Nombre: {""}
+        Nombre: {''}
         <span className="font-normal normal-case">{nombre}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Propietario: {""}
+        Propietario: {''}
         <span className="font-normal normal-case">{propietario}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Email: {""}
+        Email: {''}
         <span className="font-normal normal-case">{email}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Fecha Alta: {""}
+        Fecha Alta: {''}
         <span className="font-normal normal-case">{fecha}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Síntomas: {""}
+        Síntomas: {''}
         <span className="font-normal normal-case">{sintomas}</span>
       </p>
 
